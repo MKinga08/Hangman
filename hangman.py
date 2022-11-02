c6aff9d009558a20b2184b4a51a690bdb9440a57
MKinga08 <dr.m.kinga08@gmail.com>
2022-11-02 20:53:43 +0100

    divide major function into separate ones

--- hangman.py
+++ hangman.py
@@ -5,7 +5,7 @@ def path():
     list_of_underscores = len(word) * "_"
     list_of_words = [i for i in word]
     print(list_of_underscores)
-    return list_of_words, list_of_underscores
+    return list_of_words
 
 
 def get_users_input():
@@ -22,35 +22,40 @@ def get_users_input():
             print("You have to enter a letter")
 
 
-def game(list_of_words):
-    life = 5
-    while True:
-        guess = get_users_input()
-        for i in range(len(list_of_words)):
-            if list_of_words[i] == guess:
-                already_guessed.append(guess)
-        print(f"Already guessed letter(s):{already_guessed}")
-        print(''.join(c if c in already_guessed else '_' for c in word))
-        if len(already_guessed) == len(word):
-            print("You guessed the word!")
-            break
-        if life > 0:
-            if guess not in list_of_words:
-                life -= 1
-                if 5 >= life > 1:
-                    print(f"You have {life} lives left")
-                elif life == 1:
-                    print(f"You have {life} life left")
-        if life == 0:
-            print("You lost the game")
-            quit()
+def game(list_of_words, guess):
+    for i in range(len(list_of_words)):
+        if list_of_words[i] == guess:
+            already_guessed.append(guess)
+    print(f"Already guessed letter(s):{already_guessed}")
+    print(''.join(c if c in already_guessed else '_' for c in word))
+    if len(already_guessed) == len(word):
+        print("You guessed the word!")
+        quit()
+
+
+def lives(list_of_words, guess):
+    global life
+    if guess not in list_of_words:
+        life -= 1
+        if 5 >= life > 1:
+            print(f"You have {life} lives left")
+        elif life == 1:
+            print(f"You have {life} life left")
+    if life == 0:
+        print("You lost the game")
+        quit()
 
 
 already_guessed = []
+life = 5
+
 
 def main():
-    wordlist, fieldlist = path()
-    game(wordlist)
+    wordlist = path()
+    while True:
+        guessing = get_users_input()
+        game(wordlist, guessing)
+        lives(wordlist, guessing)
 
 
 if __name__ == "__main__":
